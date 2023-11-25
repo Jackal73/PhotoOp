c62b607f3711214479a1e2656e15fffe3fad2b28
Shawn Kebel <fxrender@outlook.com>
2023-11-25 12:26:58 -0600

    :sparkles::recycle: Refactor `import paths` and add `Tabs` component

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,11 @@ import {
   Profile,
   Saved,
   UpdateProfile,
-} from "./_root/pages";
+} from "@/_root/pages";
 import AuthLayout from "./_auth/AuthLayout";
 import RootLayout from "./_root/RootLayout";
-import SignupForm from "./_auth/forms/SignupForm";
-import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "@/_auth/forms/SignupForm";
+import SigninForm from "@/_auth/forms/SigninForm";
 import { Toaster } from "@/components/ui/toaster";
 
 import "./globals.css";
