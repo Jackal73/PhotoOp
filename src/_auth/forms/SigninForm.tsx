b2e0fcdac382f848539dbda4a3d27d6fc637ce80
Shawn Kebel <fxrender@outlook.com>
2024-05-05 11:34:56 -0500

    :art: Update `logo image paths` in SigninForm and SignupForm

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -57,11 +57,7 @@ const SigninForm = () => {
   return (
     <Form {...form}>
       <div className="sm:w-420 flex-center flex-col">
-        <img
-          src="/assets/images/PhotoOp-1.svg"
-          alt="logo"
-          className=" w-[250px]"
-        />
+        <img src="" alt="logo" className=" w-[250px]" />
 
         <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Account Sign in</h2>
         <p className="text-light-3 small-medium md:base-regular mt-2">
