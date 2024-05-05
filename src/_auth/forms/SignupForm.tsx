f57cb782968b981252f6422e11a231b77684d8b7
Shawn Kebel <fxrender@outlook.com>
2024-05-05 11:39:23 -0500

    :rocket::necktie: Update `logo image paths` in SigninForm and SignupForm

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -87,7 +87,11 @@ const SignupForm = () => {
   return (
     <Form {...form}>
       <div className="sm:w-420 flex-center flex-col">
-        <img src="" alt="logo" className=" w-[250px]" />
+        <img
+          src="/assets/images/PhotoOp-1.svg"
+          alt="logo"
+          className=" w-[250px]"
+        />
 
         <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
           Create a new account
