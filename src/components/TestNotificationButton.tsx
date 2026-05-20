09fbcfa5a1ee09a91f825a1bb42f9ec47a8fb1d7
Shawn Kebel <fxrender@outlook.com>
2026-05-19 19:04:59 -0500

    :coffin: Remove unused import of useEffect in TestNotificationButton component for cleaner code

--- src/components/TestNotificationButton.tsx
+++ src/components/TestNotificationButton.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import { testNotification } from "@/lib/appwrite/api";
 import { useUserContext } from "@/context/AuthContext";
 
